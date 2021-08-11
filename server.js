84333dab71314590ae92b57adbb0ecbdeef6d8b0
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-08-10 20:29:06 -0500

    Build database calls

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'nesneroS58',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 app.use((req, res) => {
