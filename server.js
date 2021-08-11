9f414acd074b02e8dfdb3d98680b8642232bc477
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-08-10 20:53:21 -0500

    Create query for Create Operation

--- server.js
+++ server.js
@@ -27,9 +27,39 @@ app.get('/', (req, res) => {
     });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// GET ALL CANDIDATES
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET A SINGLE CANDIDATE
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE A CANDIDATE
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// CREATE A CANDIDATE
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
+
 
 app.use((req, res) => {
     res.status(404).end();
